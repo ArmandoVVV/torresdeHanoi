c14c6d6d80dfbfbb493da73eef7520df42551cce
ArmandoVVV <73661094+ArmandoVVV@users.noreply.github.com>
2022-06-17 04:34:51 -0600

    Add files via upload

--- torresdeHanoi.asm
+++ torresdeHanoi.asm
@@ -1,6 +1,10 @@
+# practica 1 Torres de Hanoi
+# Armando Cabrales
+
+.eqv discos 3
 .data
 .text
-.eqv discos 3
+
 main:
 	lui s11, 0x10010
 	addi s1, zero, discos
@@ -10,63 +14,75 @@ main:
 crear_torres:				# s11 torre 1		s1 torre 2	s2 torre 3
 	addi t1, t1, 1
 	sw t1, 0(s11)
-	addi s11, s11, 32
+	addi s11, s11, 32		# se usan 32 por el acomodo de las torres
 	bne t1, s1, crear_torres
 	
 	addi s11, s11, -32
-	addi s1, s11, 4
+	addi s1, s11, 4			# acomodo de la torre 1 y 2
 	addi s2, s11, 8
+	
 	addi s11, zero, 0
 	lui s11, 0x10010
+	addi s11, s11, -32
 	
 	jal, ra, hanoi
-	jal zero, end
+	jal zero, exit
+
 
 hanoi:
-	addi sp, sp -4
+	addi sp, sp -4		
 	sw ra, 0(sp)
-	bne a1, t6, else
+	bne a1, t6, else	# salta si a1 no es igual a 1
 	
-	#caso base
-	
-	
-	sw zero, 0(s11)
-	sw a1, 0(s2)
-	addi s11, s11, 32
+	# caso base
+	addi s11, s11, 32		
+	sw zero, 0(s11)		# remueve el disco de arriba de la torre de origen
+	sw a1, 0(s2)		# añade el disco a la torre destino actual
 	addi s2, s2, -32
 	
-	jalr, zero, ra, 0
-	
+	jal zero, return_hanoi	# carga a ra el valor de retorno
 	
 else:
-	addi a1, a1, -1		#intercambio
-	add t2, s1, zero
-	add s1, s2, zero
-	add s2, t2, zero
-	
-	jal ra, hanoi
-	
-	
-	sw zero, 0(s11)
-	sw a1, 0(s2)
-	addi s11, s11, 32
-	addi s2, s2, -32
+	add t2, zero, s1	# intercambia la direccion de memoria de las torres
+	add s1, zero, s2	# torre auxiliar = torre destino
+	add s2, zero, t2	# torre destino = torre auxiliar
 	
 	addi a1, a1, -1
-	add t2, s1, zero
-	add s1, s11, zero
-	add s11, t2, zero
+	jal ra, hanoi		# vuelve a llamar con n - 1
+				
+	addi a1, a1, 1		
+	add t2, zero, s1	# intercambia la direccion de memoria de las torres
+	add s1, zero, s2	# torre auxiliar = torre destino
+	add s2, zero, t2	# torre destino = torre auxiliar
+				
+	# regresa a la normalidad
 	
-	jal ra, hanoi
+	addi s11, s11, 32
+	sw zero, 0(s11)		# remueve el disco de arriba de la torre de origen
+	sw a1, 0(s2)		# añade el disco a la torre auxiliar actual
+	addi s2, s2, -32
 	
-return_hanoi:
-	lw ra, 0(sp)
-	addi sp, sp, 4
+	
+	add t0, zero, s11	# intercambia la direccion de memoria de las torres
+	add s11, zero, s1	# torre inicial = torre auxiliar
+	add s1, zero, t0	# torre auxiliar = torre inicial
+	
+	addi a1, a1, -1
+	jal ra, hanoi		# vuelve a llamar con n - 1
+	
 	addi a1, a1, 1
+	add t0, zero, s11	# intercambia la direccion de memoria de las torres
+	add s11, zero, s1	# torre inicial = torre auxiliar
+	add s1, zero, t0	# torre auxiliar = torre inicial
+	
+	# regresa a la normalidad
+
+return_hanoi:
+	lw ra, 0(sp)		# carga a ra el valor de retorno almacenado en el stack
+	addi sp, sp, 4		
 	jalr zero, ra, 0
-	
-end: add zero, zero, zero
-	
+
+exit:
 	
 	
 	
